77b2c7816e6b8f6b4525025d047c73b2c0fa0a58
harshalsuryawanshi23 <harshalsuryawanshi2308@gmail.com>
2026-09-21 13:30:07 +0530

    Time: 4 ms (56.91%) | Memory: 26.7 MB (25.56%) - LeetSync

---
+++ 680-valid-palindrome-ii/valid-palindrome-ii.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool ispl(string s,int st, int ed){
+        while(st < ed){
+            if(s[st++] != s[ed--]) return false;
+        }
+        return true;
+    }
+    bool validPalindrome(string s) {
+        int st = 0;
+        int ed = s.length()-1;
+        while(st < ed){
+            if(s[st] != s[ed]){
+                return ispl(s,st+1,ed) || ispl(s,st,ed-1);
+            }
+            st++;
+            ed--;
+        }
+        return true;
+    }
+};
